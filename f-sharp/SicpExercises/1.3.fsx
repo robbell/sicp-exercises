083a779c4ef71866a5a945939111e94a16875125
Rob Bell <me@rob-bell.net>
2018-01-05 23:24:49 +0000

    Fix conditional bug in F# 1.3

--- f-sharp/SicpExercises/1.3.fsx
+++ f-sharp/SicpExercises/1.3.fsx
@@ -1,10 +1,10 @@
 // Exercise 1.3
 
-let square a = a * a
-
-let sumOfSquares a b = square a + square b
-
 let sumSquaresOfTwoLargest a b c = 
-    if a > b then sumOfSquares a c
-    else if a > c then sumOfSquares a b
-    else sumOfSquares b c
+    let square a = a * a
+    let sumOfSquares a b = square a + square b
+
+    if a > b then sumOfSquares a (if b > c then b else c)
+    else sumOfSquares b (if a > c then a else c)
+
+sumSquaresOfTwoLargest 1 2 3 // 2^2 + 3^2 = 13
