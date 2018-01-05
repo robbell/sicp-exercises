c3540a785ad825790555d25170d4938a11884063
Rob Bell <me@rob-bell.net>
2018-01-05 23:00:22 +0000

    Add answers for exercise 1.1 in F#

---
+++ f-sharp/SicpExercises/1.1.fsx
@@ -0,0 +1,31 @@
+// Exercise 1.1
+
+10
+
+5 + 3 + 4
+
+9 - 1
+
+6 / 2
+
+(2 * 4) + (4 - 6)
+
+let a = 3
+
+let b = a + 1
+
+a + b + (a * b)
+
+a = b
+
+if b > a && b < a * b then b else a
+
+if a = 4 then 6 
+else if b = 4 then 6 + 7 + a
+else 25
+
+2 + if b > a then b else a
+
+(if a > b then a
+else if a < b then b
+else -1) * (a + 1)
