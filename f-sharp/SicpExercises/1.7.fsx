bf9729ebc873a818b7dc984ab1a00828c548f2f9
Rob Bell <me@rob-bell.net>
2018-01-14 01:29:57 +0000

    Improve sqrt to wait for no change in improved guess

--- f-sharp/SicpExercises/1.7.fsx
+++ f-sharp/SicpExercises/1.7.fsx
@@ -8,16 +8,19 @@ let improve guess radicand = average guess (radicand / guess)
 
 let goodEnough guess radicand = abs (square guess - radicand) < 0.001
 
-let improvedGoodEnough guess radicand = abs (guess - improve guess radicand) < 0.001
+let goodEnoughImproved guess radicand = abs (guess - improve guess radicand) < 0.001
+
+let goodEnoughExact guess radicand = guess = improve guess radicand
 
 let rec sqrtIter guess radicand = 
-    if improvedGoodEnough guess radicand then guess 
+    if goodEnoughExact guess radicand then guess 
     else sqrtIter (improve guess radicand) radicand
 
 let sqrt radicand = sqrtIter 1.0 radicand
 
 sqrt 144.0
 
-// sqrt 10000000000000.0    // orig: infinite loop  improved: 3162277.66    actual: 3162277.66
-// sqrt 0.0001              // orig: 0.03230844833  improved: 0.01012021837 actual: 0.01
-// sqrt 144.0               // orig: 12.00000001    improved: 12.00054573   actual: 12.0
+// function call            original        improved        exact       actual
+// sqrt 10000000000000.0    infinite loop   3162277.66      3162277.66  3162277.66
+// sqrt 0.0001              0.03230844833   0.01012021837   0.01        0.01
+// sqrt 144.0               12.00000001     12.00054573     12.0        12.0
