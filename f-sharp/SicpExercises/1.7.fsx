1c66219979d70657ba2e61d692469136c9c51d12
Rob Bell <me@rob-bell.net>
2018-01-14 01:06:21 +0000

    Add improved goodEnough implementation

--- f-sharp/SicpExercises/1.7.fsx
+++ f-sharp/SicpExercises/1.7.fsx
@@ -8,10 +8,16 @@ let improve guess radicand = average guess (radicand / guess)
 
 let goodEnough guess radicand = abs (square guess - radicand) < 0.001
 
+let improvedGoodEnough guess radicand = abs (guess - improve guess radicand) < 0.001
+
 let rec sqrtIter guess radicand = 
-    if goodEnough guess radicand then guess 
+    if improvedGoodEnough guess radicand then guess 
     else sqrtIter (improve guess radicand) radicand
 
 let sqrt radicand = sqrtIter 1.0 radicand
 
-sqrt 144.0 // 12.00000001
+sqrt 144.0
+
+// sqrt 10000000000000.0    // orig: infinite loop  improved: 3162277.66    actual: 3162277.66
+// sqrt 0.0001              // orig: 0.03230844833  improved: 0.01012021837 actual: 0.01
+// sqrt 144.0               // orig: 12.00000001    improved: 12.00054573   actual: 12.0
