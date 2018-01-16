00c4207436453a4189b50e4b13ceb0c0a7544a90
Rob Bell <rob.bell@asos.com>
2018-01-16 13:30:21 +0000

    Add first part of exercise 1.9 solution in F#

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
-SICP Exercises
-==============
+# SICP Exercises
+
 My answers to the exercises from the book ["Structure and Interpretation of Computer Programs"](http://mitpress.mit.edu/sicp/full-text/book/book.html).
 
-Completed:
+## Completed exercises
+
+F#:
+
+* 1.2
+* 1.3
+* 1.7
+* 1.8
+* 1.9
+
+Scheme:
 
 * 1.2
 * 1.3
---
+++ f-sharp/SicpExercises/1.9.fsx
@@ -0,0 +1,36 @@
+// Exercise 1.9
+
+// (define (+ a b)
+//   (if (= a 0) 
+//       b 
+//       (inc (+ (dec a) b))))
+
+let inc x = x + 1
+
+let dec x = x - 1
+
+let inline (+) a b = 
+    if a = 0 then b
+    else inc((dec a) + b)
+
+// 4 + 5
+// inc((dec 4) + 5)
+// inc(3 + 5)
+// inc(inc((dec 3) + 5))
+// inc(inc(2 + 5))
+// inc(inc(inc((dec 2) + 5)))
+// inc(inc(inc(1 + 5)))
+// inc(inc(inc(inc((dec 1) + 5))))
+// inc(inc(inc(inc(0 + 5))))
+// inc(inc(inc(inc(5))))
+// inc(inc(inc(6)))
+// inc(inc(7))
+// inc(8)
+// 9
+
+4 + 5
+
+// (define (+ a b)
+//   (if (= a 0) 
+//       b 
+//       (+ (dec a) (inc b))))
